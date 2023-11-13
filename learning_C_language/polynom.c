59bceb0be435d3fcd15e98e341cea9f01db011c9
Sourr_cream <roman_boyko.48@mail.ru>
2023-11-14 00:26:15 +0300

    edit 1.1

--- learning_C_language/polynom.c
+++ learning_C_language/polynom.c
@@ -1,20 +1,40 @@
 #include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+long long calculate_px0(long long arr[], long long length, long long x0)
+{
+    long long px0 = arr[0];
+    for(long long i = 0; i < length; i++) {
+        px0 = px0 * x0 + arr[i + 1];
+    }
+    return px0;
+}
+
+long long calculate_pdx0(long long arr[], long long length, long long x0)
+{
+    for(long long i = 0; i < length; i++) {
+        arr[i] = (length - i) * arr[i];
+    }
+    long long pdx0 = arr[0];
+    for(long long i = 0; i < length - 1; i++) {
+        pdx0 = pdx0 * x0 + arr[i + 1];
+    }
+    return pdx0;
+}
+
 
 int main() {
-    int n, x0;
-    int px0 = 0, pdx0 = 0;
-    scanf("%d %d", &n, &x0);
-    int arr[n + 1];
-    for(int i = 0; i <= n; i++) {
-        scanf("%d", &arr[i]);
-        px0 += arr[i];
-        if(i != n) px0 *= x0;
-    }
-    for(int i = 0; i < n; i++) {
-        pdx0 += (n - i) * arr[i];
-        if(i != n - 1) pdx0 *= x0;
-    }
+    long long n, x0;
+    scanf("%lld %lld", &n, &x0);
+    long long arr[n + 1];
 
-    printf("%d %d", px0, pdx0);
+    // scanning array of elements
+    for(long long i = 0; i <= n; i++) scanf("%lld", &arr[i]);
+
+    long long px0 = calculate_px0(arr, n, x0);
+    long long pdx0 = calculate_pdx0(arr, n, x0);
+
+    printf("%lld %lld", px0, pdx0);
     return 0;
 }
