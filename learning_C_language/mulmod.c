f0838a067cd8bb7cf00c0dc229642937b3de3f80
Sourr_cream <roman_boyko.48@mail.ru>
2023-11-14 06:47:51 +0300

    edit 1.2

--- learning_C_language/mulmod.c
+++ learning_C_language/mulmod.c
@@ -1,50 +1,26 @@
 #include <stdio.h>
 
-int dec2bin(int num)
+unsigned long long mulmod(unsigned long long a, unsigned long long b, unsigned long long m)
 {
-    int bin = 0, k = 1;
-    while (num) {
-        bin += (num % 2) * k;
-        k *= 10;
-        num /= 2;
+    unsigned long long bin[64];
+    int i = 0;
+    while (b) {
+        bin[i] = b % 2;
+        b /= 2;
+        i++;
     }
-    return bin;
-}
-
-int get_len_of_number(long long num)
-{
-    int len = 1;
-    while(num / 10) {
-        len++;
-        num /= 10;
+    i--;
+    unsigned long long res = (a % m) * (bin[i] % m);
+    for(int k = i - 1; k >= 0; k--) {
+        res = (res % m) * 2;
+        res = ((res % m) + ((a % m) * (bin[k] % m)) % m) % m;
     }
-    return len;
-}
- 
-
-int mulmod(int a, int b, int m)
-{
-    long long bin_b = dec2bin(b);
-    int len_bin_b = get_len_of_number(bin_b);
-    int res = 0;
-    int bArray[len_bin_b];
-    long long new_b = bin_b;
-    int j = len_bin_b - 1;
-    while(new_b != 0) {
-        bArray[j] = new_b % 10;
-        new_b /= 10;
-        j--;
-    }
-    for(int i = 0; i < len_bin_b; i++) {
-        res = (res % m + ((a % m) * (bArray[i] % m) % m)) % m;
-        if (i != len_bin_b - 1) res = ((res % m) * (2 % m)) % m;
-    }
-    return res % m;
+    return res;
 }
 
 int main() {
-    int a, b, m;
-    scanf("%d %d %d", &a, &b, &m);
-    printf("%d", mulmod(a, b, m));
+    unsigned long long a, b, m;
+    scanf("%lld %lld %lld", &a, &b, &m);
+    printf("%lld", mulmod(a, b, m));
     return 0;
 }
