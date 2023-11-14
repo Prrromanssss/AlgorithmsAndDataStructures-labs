7b6aa00c673c9939a938004bbd6789a2ef558801
Sourr_cream <roman_boyko.48@mail.ru>
2023-11-15 00:00:16 +0300

    edit 1.5

--- learning_C_language/permut.c
+++ learning_C_language/permut.c
@@ -34,13 +34,16 @@ bool permut(int a[], int b[], int start, int end, size_t length, int *is_same)
 
 int main() 
 {
-    int is_false = 0;
-    int* p = &is_false;
+    int is_same = 0;
+    int* p = &is_same;
     int n = 8;
     int nums1[n], nums2[n];
+	
+	// scanning arrays
 	for(int i = 0; i < n; i++) scanf("%d", &nums1[i]);
     for(int i = 0; i < n; i++) scanf("%d", &nums2[i]);
-	bool res = permut(nums1, nums2, 0, n - 1, 4, p);
+
+	bool res = permut(nums1, nums2, 0, n - 1, n, p);
     if(res) printf("yes");
     else printf("no");
 	return 0;
