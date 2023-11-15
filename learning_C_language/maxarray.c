9a84d353bb36ed0a4d0806bce84f4da2e654c8ba
Sourr_cream <roman_boyko.48@mail.ru>
2023-11-16 01:53:35 +0300

    1.10

--- learning_C_language/maxarray.c
+++ learning_C_language/maxarray.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
+
+int maxarray(void *base, size_t nel, size_t width,
+        int(*compare)(void *a, void *b))
+{
+    uint8_t *p_max_elem = malloc(width);
+    uint8_t *elem = base;
+    int max_index = 0;
+    memcpy(p_max_elem, base, width);
+    for(int i = 1; i < nel; i++) {
+        elem += width;
+        int res = compare(elem, p_max_elem);
+        if(res > 0) {
+            max_index = i;
+            memcpy(p_max_elem, elem, width);
+        }
+    }
+    free(p_max_elem);
+    return max_index;
+}
 
 int scan_array(int arr[], int n)
 {
@@ -19,25 +39,6 @@ int compare(void *a, void *b)
 	return 0;
 }
 
-int maxarray(void *base, size_t nel, size_t width,
-        int(*compare)(void *a, void *b))
-{
-    char *p_max_elem = malloc(width);
-    int max_index = 0;
-    memcpy(p_max_elem, base, width);
-    for(int i = 0; i < nel; i++) {
-        char *elem = base + i * width;
-        int res = compare(elem, p_max_elem);
-        if(res > 0) {
-            max_index = i;
-            memcpy(p_max_elem, elem, width);
-        }
-    }
-
-    free(p_max_elem);
-    return max_index;
-}
-
 int main()
 {
     int n;
@@ -45,6 +46,5 @@ int main()
     int arr[n];
     *arr = scan_array(arr, n);
     printf("%d", maxarray(arr, n, sizeof(int), compare));
-
     return 0;
 }
